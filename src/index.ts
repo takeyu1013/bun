44fc94f07bedd1e3f9273e4bf50ef6aa6015be71
takeyu1013 <yuto1013@gmail.com>
2023-11-30 14:16:56 +0000

    ⚗️

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
-  .use(swagger())
+  // .use(swagger())
   .get("/", ({ body, set }) => {
     console.log(body);
     set.status = 200;
     return "Hello Elysia";
   })
   .get("/ping", () => "pong")
-  .get("id/:id", ({ params: { id } }) => `${id}`)
+  .get("id/:id", ({ params: { id } }) => id)
+  .post("/mirror", ({ body }) => body, {
+    body: t.Object({
+      id: t.Number(),
+      name: t.String(),
+    }),
+  })
   .listen(3000);
+
+export type App = typeof app;
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
