86e7080b5df2691f814dfb03b547f2fcd894db96
takeyu1013 <yuto1013@gmail.com>
2023-12-20 11:21:31 +0000

    ✨

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/hello", () => "Hello Elysia").listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export type App = typeof app;
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
+import type { App } from "backend/src";
+
+import { useLoaderData } from "@remix-run/react";
+import { edenTreaty } from "@elysiajs/eden";
+
+export const loader = async () => {
+  const client = edenTreaty<App>("http://localhost:8080");
+  return client.hello.get();
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +17,12 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { data } = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1>Home</h1>
+      <p>{data}</p>
+    </>
   );
 }
