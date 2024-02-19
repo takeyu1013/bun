88ed05234dc1d6b2c47f4ef91b5ab8ea11a04178
takeyu1013 <yuto1013@gmail.com>
2024-02-19 12:04:04 +0000

    ✨

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,16 @@
+import { LinksFunction } from "@remix-run/node";
 import {
+  Form,
   Links,
   Meta,
-  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import href from "./app.css?url";
+
+export const links = (() => [
+  { rel: "stylesheet", href },
+]) satisfies LinksFunction;
 
 export default function App() {
   return (
@@ -16,7 +22,34 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="/contacts/1">Your Name</a>
+              </li>
+              <li>
+                <a href="/contacts/2">Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
